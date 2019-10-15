6dc0f09514ddd29c4ee7b0aad1f0911da2ee8687
nanqi <luyu_1216@126.com>
2019-10-15 13:10:43 +0800

    modify;

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const router = require('tcb-router');
+const cloud = require('wx-server-sdk');
+
+cloud.init()
+
 const db = require('./db');
 const format = require('./format');
 
 module.exports = {
   router,
   db,
-  format
+  format,
+  cloud
 }
